b3d3c55655fa1d1f84bb5c3268cd22601e555526
nabil19 <funity111@gmail.com>
2024-07-29 20:51:19 +0100

    new me

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  esbuild: {
-    loader: 'jsx',
-    include: [
-      // Add this for `node_modules` resolution
-      'src/**/*.jsx',
-      'node_modules/**/*.jsx'
-    ],
-    exclude: [],
-  },
-  server: {
-    mimeTypes: {
-      // Ensure the server responds with the correct MIME type
-      'jsx': 'application/javascript',
-      'js': 'application/javascript',
-    },
-  },
-});
+})
